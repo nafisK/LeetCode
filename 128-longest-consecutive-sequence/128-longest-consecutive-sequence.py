6f8b01c53f41ffddbad0b71227040c0f2d98e265
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-06-22 17:20:12 -0400

    Time: 435 ms (68.33%), Space: 28.5 MB (37.43%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,30 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
-        # Brute Force
+       # Efficient
+        nums = set(nums)
+        max_length = 0
+        # iterate through nums
+        # if you find a start,
+            # Keep iterating until you find the last consec num from that start in the set
+                # iterate start by one until you hit the end | lets name it end
+            # get the diff of end - start
+        # check if output of current consec is greater than last consec
         
-        # edge-case
-        if not nums:
-            return 0
-        
-        # makes num a set -> turns the set back into a list -> sorts the list -> saves the list
-        nums = sorted(list(set(nums)))
-        
-        # sofar: keeps track of the length, output: 
-        sofar, output = 1, 1
-        
-        # iterate from 2nd element to end
-        for i in range(1, len(nums)):
-            
-            # check prev. element with curr element
-            if nums[i - 1] + 1 == nums[i]:
-                # increment with consec
-                sofar += 1
-            else:
-                # reset sofar if not consec
-                sofar = 1
-            
-            # after each iteration, find the new max and set to output
-            output = max(output ,sofar)
-        
-        return output
+        for n in nums:
+            if n - 1 not in nums:
+                start = n
+                while start in nums:
+                    start += 1
+                    
+                max_length = max (max_length, start - n)
+                
+        return max_length
